14ab29b0081d621aa3800e4fa67035edcc1c85a6
Cesar Melendez <cesarf.melendez@gmail.com>
2023-07-19 22:07:47 -0400

    Initial Commit

---
+++ WaterWand1/WaterWand1App.swift
@@ -0,0 +1,17 @@
+//
+//  WaterWand1App.swift
+//  WaterWand1
+//
+//  Created by Cesar Melendez on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct WaterWand1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
